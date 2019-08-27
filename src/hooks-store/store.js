af69e0eca5f94c458284ab0270ec38980a68ae88
Dieter Doman <dieter@jemstep.com>
2019-08-27 10:59:10 +0200

    Minor performance optimization.

--- src/hooks-store/store.js
+++ src/hooks-store/store.js
@@ -4,7 +4,7 @@ let globalState = {};
 let listeners = [];
 let actions = {};
 
-export const useStore = () => {
+export const useStore = (shouldListen =  true) => {
     const setState = useState(globalState)[1];
 
     const dispatch = (actionIdentifier, payload) => {
@@ -17,11 +17,13 @@ export const useStore = () => {
     };
 
     useEffect(() => {
-        listeners.push(setState);
-        return () => {
-            listeners = listeners.filter(li => li !== setState);
-        };
-    }, [setState]);
+        if (shouldListen) {
+            listeners.push(setState);
+            return () => {
+                listeners = listeners.filter(li => li !== setState);
+            };
+        }
+    }, [setState, shouldListen]);
 
     return [globalState, dispatch];
 };
